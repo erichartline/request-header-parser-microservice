e7b080cc64f72e28fb4e647639bd2da6fc26ae68
wildlifehexagon <crossthebreeze@gmail.com>
2017-03-18 13:03:40 -0500

    shortened data strings and updated readme

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const port = process.env.PORT || 3000;
 app.get('/', function(req, res) {
   res.send({
     ipaddress: req.header('x-forwarded-for'),
-    language: req.header('accept-language'),
-    software: req.header('user-agent')
+    language: req.header('accept-language').split(',')[0],
+    software: req.header('user-agent').split(/[()]+/)[1]
   });
 });
 
